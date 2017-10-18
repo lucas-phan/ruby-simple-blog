120d52c966795deb909f03f18941b6a1a1cd4171
lucas-phan <chinhdung180@gmail.com>
2017-10-18 10:03:43 +0700

    finish project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "chinhdung", password: "12345", only: [:destroy]
     def create
-        @post = Post.find(params[:id])
+        @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_pramas)
         redirect_to post_path(@post)
 
+    end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 
     private def comment_pramas
